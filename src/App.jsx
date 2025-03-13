7c438f02d6583fd42810d60333b0ff64a0009791
Youssef.Nassar <yossef41997@gmail.com>
2025-03-14 00:36:02 +0300

    create the header the update message section and the languages chips

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react"
+import { languages } from "../public/languages"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function AssemblyEndgame() {
+  const languageElements = languages.map(lang => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color
+    }
+    return (
+      <span
+        className="chip"
+        style={styles}
+        key={lang.name}
+      >
+        {lang.name}
+      </span>
+    )
+  })
 
   return (
-    <>
-      <h1>Assembly Endgame</h1>
-    </>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word within 8 attempts to keep the
+          programming world safe from Assembly!</p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">
+        {languageElements}
+      </section>
+    </main>
   )
 }
-
-export default App
